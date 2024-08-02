a96773a9272d2a19298b4e1705eeda43cbed7395
FTS427 <FTS427@outlook.com>
2024-08-03 02:07:19 +0800

    M | anykernel config | partition

--- anykernel.sh
+++ anykernel.sh
@@ -29,7 +29,7 @@ set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 } # end attributes
 
 # boot shell variables
-block= /dev/block/mmcblk0p31;
+block=boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 patch_vbmeta_flag=auto;
